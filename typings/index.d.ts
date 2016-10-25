f8e5ac619079cc8f3a56f3ead4d770cefd4141ad
Ivaylo <ivaylo.kenov@gmail.com>
2016-10-25 19:08:50 +0300

    Added IntelliSense and VS config files

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/express-serve-static-core/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/serve-static/index.d.ts" />
